80e8b3f0772b0741dafd9cd30ea26f80cdb1b1b8
idolago <idolago94@gmail.com>
2020-03-25 17:09:16 +0200

    photo syntax

--- app/components/Photo/Photo.js
+++ app/components/Photo/Photo.js
@@ -231,17 +231,14 @@ export default class Photo extends Component {
             hearts={imageData.hearts}
             onSymbol={() => NavigationStore.navigate(Routes.Screens.PROFILE.routeName, {id: userData._id})}
           />
-
+          {/* emoji box */}
           <View style={styles.emoji}>
-            {
-              !openEmoji ? null :
-                <EmojiBox 
-                  includeHeart={true} 
-                  emojiSize={this.emojiBoxSize} 
-                  heartPress={this.heartPress.bind(this)} 
-                  emojiPress={this.emojiPress.bind(this)}
-                />
-            }
+            {openEmoji && <EmojiBox 
+              includeHeart={true} 
+              emojiSize={this.emojiBoxSize} 
+              heartPress={this.heartPress.bind(this)} 
+              emojiPress={this.emojiPress.bind(this)}
+            />}
           </View>
           {/* emoji sent */}
           <Animated.Image
@@ -278,31 +275,25 @@ export default class Photo extends Component {
             <Icon color={Style.colors.heart} name={iconNames.FULL_HEART} size={this.emojiBoxSize} />
           </Animated.View>
           {/* sparkle animation */}
-          {
-            this.sparkleAnimation.map((anim, i) => (
-              <Animated.Image key={i} source={require('../../assets/sparkle.gif')} style={{
-                position: 'absolute',
-                width: this.sizeEmoji,
-                height: this.sizeEmoji,
-                opacity: this.fadeEmoji,
-                top: anim.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [emojiSendPosition.y, 0],
-                }),
-                left: anim.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [emojiSendPosition.x, Dimensions.get('window').width * 0.77],
-                }),
-              }}/>
-            ))
-          }
-          <View style={styles.buttonsBox}>
-          </View>
+          {this.sparkleAnimation.map((anim, i) => (
+            <Animated.Image key={i} source={require('../../assets/sparkle.gif')} style={{
+              position: 'absolute',
+              width: this.sizeEmoji,
+              height: this.sizeEmoji,
+              opacity: this.fadeEmoji,
+              top: anim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [emojiSendPosition.y, 0],
+              }),
+              left: anim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [emojiSendPosition.x, Dimensions.get('window').width * 0.77],
+              }),
+            }}/>
+          ))}
         </View>
-        <Buttons 
-          onOpenEmoji={() => this.toggleEmoji()}
-        />
-
+        <Buttons onOpenEmoji={() => this.toggleEmoji()} />
+        {/* comments */}
         <View style={styles.commentsBox}>
           <View style={{flexDirection: 'row', marginBottom: 5}}>
             <Text style={styles.username}>{userData.username}: </Text>
@@ -332,11 +323,6 @@ const styles = StyleSheet.create({
     backgroundColor: Style.colors.background,
     position: 'relative',
   },
-  userName: {
-    fontSize: 16,
-    color: Style.colors.text,
-    fontWeight: 'bold',
-  },
   photoBox: {
     width: Dimensions.get('window').width,
     aspectRatio: 1,
@@ -346,57 +332,8 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
-  photoEarn: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    margin: 20,
-    borderRadius: 999,
-    backgroundColor: 'rgba(128, 128, 128, 0.4)',
-    flexDirection: 'row-reverse',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    maxHeight: 22,
-  },
-  iconBagBox: {
-    padding: 5,
-    backgroundColor: Style.colors.darkMain,
-    borderRadius: 999,
-  },
-  number: {
-    color: Style.colors.text,
-    paddingHorizontal: 8,
-  },
-
-  buttonsBox: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: Dimensions.get('window').width,
-    marginTop: 10,
-    alignItems: 'center'
-  },
-  leftSide: {
-    flexDirection: 'row',
-  },
   icon: {
     padding: 10,
-  },
-  iconBox: {
-    margin: 5,
-    borderRadius: 999,
-    borderWidth: 1,
-    borderColor: Style.colors.text,
-  },
-
-  anotherPhotos: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    opacity: 0.6,
-  },
-  smallPhoto: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
   },
 
   username: {
