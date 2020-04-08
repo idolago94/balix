6981e0166e7d968f9198958a0c4ef3f15ae41175
idolago <idolago94@gmail.com>
2020-04-08 17:26:04 +0300

    mute video at start

--- app/components/ProgressiveImage/PreogressiveImage.js
+++ app/components/ProgressiveImage/PreogressiveImage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 // Componenta
-import {StyleSheet, Text, View, Image, ScrollView, TouchableHighlight, Animated, Dimensions, Alert} from 'react-native';
+import {StyleSheet, Text, View, Image, ScrollView, TouchableWithoutFeedback, Animated, Dimensions, Alert} from 'react-native';
 import DoubleClick from 'react-native-double-click';
 import Video from 'react-native-video';
 
@@ -9,6 +9,9 @@ export default class ProgressiveImage extends Component {
   constructor(props) {
     console.log('ProgressiveImage -> constructor');
     super(props);
+    this.state={
+      videoMuted: true
+    }
     this.imageLoad = new Animated.Value(0);
   }
 
@@ -30,10 +33,13 @@ export default class ProgressiveImage extends Component {
                   onLoad={() => this.onImageLoad()}
                 />
               ):(
-                <Video 
-                  style={[this.props.style]}
-                  source={{uri: this.props.url}}
-                />
+                <TouchableWithoutFeedback onPress={() => this.setState({videoMuted: !this.state.videoMuted})}>
+                  <Video 
+                    style={[this.props.style]}
+                    source={{uri: this.props.url}}
+                    muted={this.state.videoMuted}
+                  />
+                </TouchableWithoutFeedback>
               )}
             </DoubleClick>
         )
@@ -47,10 +53,13 @@ export default class ProgressiveImage extends Component {
               onLoad={() => this.onImageLoad()}
           />
         ):(
-          <Video 
-            style={[this.props.style]}
-            source={{uri: this.props.url}}
-          />
+          <TouchableWithoutFeedback onPress={() => this.setState({videoMuted: !this.state.videoMuted})}>
+            <Video 
+              style={[this.props.style]}
+              source={{uri: this.props.url}}
+              muted={this.state.videoMuted}
+            />
+          </TouchableWithoutFeedback>
         )}
       </View>
     )
