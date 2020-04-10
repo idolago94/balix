e398a0effb0f0f423960de224320a6a5ae5af2e1
idolago <idolago94@gmail.com>
2020-04-10 23:36:06 +0300

    photo indicators with line

--- app/components/Photo/PhotoIndicator.js
+++ app/components/Photo/PhotoIndicator.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Icon, {iconNames} from '../Icon/Icon';
-import ProfileSymbol from '../ProfileSymbol/ProfileSymbol';
 import { colors } from '../../utils/style';
 import { thousandsWithCommas } from '../../utils/Tools';
 
@@ -10,48 +9,16 @@ export default function PhotoIndicator(props) {
 
     return (
       <View style={{position: 'absolute', right: 0, alignItems: 'flex-start'}}>
-        {/* <ProfileSymbol 
-          style={{marginLeft: 10,marginTop: 10, borderWidth: 1, borderColor: 'black', borderRadius: 999}} 
-          src={props.user.profileImage} 
-          size={55}
-          press={() => props.onSymbol()}
-        /> */}
         <View style={{justifyContent: 'center', alignItems: 'center', margin: 5, marginLeft: 10, padding: 4}}>
-          <Icon color={colors.text} name={iconNames.DOLLAR} size={22} />
+          {/* <Icon color={colors.text} name={iconNames.DOLLAR} size={22} /> */}
+          <Image style={{height: 22, aspectRatio: 1}} source={require('../../assets/dollar_indicator.png')} />
           <Text style={{color: colors.text}}>{thousandsWithCommas(props.cash)}</Text>
         </View>
         <View style={{justifyContent: 'center', alignItems: 'center', margin: 5, marginLeft: 10, padding: 4}}>
-          <Icon color={colors.text} name={iconNames.FULL_HEART} size={22} />
+          {/* <Icon color={colors.text} name={iconNames.FULL_HEART} size={22} /> */}
+          <Image style={{height: 22, aspectRatio: 1}} source={require('../../assets/heart_indicator.png')} />
           <Text style={{color: colors.text}}>{thousandsWithCommas(props.hearts)}</Text>
         </View>
       </View>
     );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: -27,
-    right: 0,
-    flexDirection: 'row',
-    backgroundColor: 'rgba(128, 128, 128, 0.4)',
-    alignItems: 'center',
-    padding: 3,
-  },
-  indicatorBox: {
-    flexDirection: 'row-reverse',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginHorizontal: 5
-  },
-  iconBox: {
-    paddingVertical: 3,
-    borderRadius: 999
-  },
-  number: {
-    color: 'white',
-    // fontWeight: 'bold',
-    fontSize: 15
-  },
-
-});
