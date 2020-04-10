af9c187bd63b578d074e81ae560ce4853fcad077
idolago <idolago94@gmail.com>
2020-04-11 00:05:59 +0300

    profile symbol syntax

--- app/components/ProfileSymbol/ProfileSymbol.js
+++ app/components/ProfileSymbol/ProfileSymbol.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import Icon, { iconNames } from '../Icon/Icon';
 import LiveIndicator from './LiveIndicator';
 import ProgressiveImage from '../ProgressiveImage/PreogressiveImage';
@@ -32,50 +32,27 @@ export default function ProfileSymbol(props) {
 
     const iconPosition = calculateIconLocation(props.iconDeg || 145);
     return (
-        <View style={{...props.style, alignItems: 'center', position: 'relative'}}>
-          <View style={{position: 'relative'}}>
-          {
-            (props.press) ?
-            (
-            <TouchableHighlight style={[(props.story || props.live) ? (styles.storyBorder):({})]} onPress={imageClicked.bind(this)}>
-                <ProgressiveImage 
-                  style={[styles.image, {height: props.size, width: props.size}]}
-                  url={props.src || 'non-profile'}
-                />
-            </TouchableHighlight>
-            ) :
-            (
-              <View style={[(props.story || props.live) ? (styles.storyBorder):({})]}>
-                <ProgressiveImage 
-                  style={[styles.image, {height: props.size, width: props.size}]}
-                  url={props.src || 'non-profile'}
-                />
-              </View>
-            )
-          }
-          {props.icon && (
-              (props.iconPress) ?
-              (
-                <TouchableHighlight
-                  style={[styles.icon, {top: iconPosition.y, left: iconPosition.x, padding: props.iconSize*0.5 || (props.size/5.5)*0.5}]}
-                  onPress={props.iconPress.bind(this)}
-                >
-                  <Icon
-                    size={props.iconSize || props.size/5.5}
-                    name={props.icon}
-                    color={props.iconColor || 'black'}
-                  />
-                </TouchableHighlight>
-              ) :
-              (
-                <Icon
-                  style={[styles.icon, {top: iconPosition.y, left: iconPosition.x, padding: props.iconSize*0.5 || (props.size/5.5)*0.5}]}
-                  size={props.iconSize || props.size/5.5}
-                  name={props.icon}
-                  color={props.iconColor || 'black'}
-                />
-              ))
-          }
+        <View style={{alignItems: 'center'}}>
+          <View>
+            <TouchableOpacity 
+              style={[props.style, (props.story || props.live) ? (styles.storyBorder):({})]} 
+              onPress={props.press ? (e => props.press(e)):(() => console.log('symbol press'))}
+            >
+              <ProgressiveImage 
+                style={[styles.image, {height: props.size, width: props.size}]}
+                url={props.src || 'non-profile'}
+              />
+            </TouchableOpacity>
+            {props.icon && <TouchableOpacity
+              style={[styles.icon, {top: iconPosition.y, left: iconPosition.x, padding: props.iconSize*0.5 || (props.size/5.5)*0.5}]}
+              onPress={props.iconPress ? (props.iconPress.bind(this)):(() => console.log('icon press'))}
+            >
+              <Icon
+                size={props.iconSize || props.size/5.5}
+                name={props.icon}
+                color={props.iconColor || 'black'}
+              />
+            </TouchableOpacity>}
           </View>
           {props.live && <LiveIndicator press={imageClicked.bind(this)} size={props.size} />}
         </View>
@@ -86,20 +63,5 @@ const styles = StyleSheet.create({
   image: {
     borderRadius: 999,
     backgroundColor: 'gray'
-  },
-  cashBox: {
-    flexDirection: 'row',
-    justifyContent: 'center'
-  },
-  cash: {
-    color: colors.text
-  },
-  icon: {
-    position: 'absolute',
-    // padding: 12, 
-    borderRadius:999, 
-    backgroundColor: colors.text,
-    borderColor: colors.background,
-    borderWidth: 3
   }
 });
