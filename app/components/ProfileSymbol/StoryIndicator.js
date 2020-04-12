284a33769af3f85f28b27402570022e66d207276
idolago <idolago94@gmail.com>
2020-04-12 19:43:21 +0300

    delete not use modules

--- app/components/ProfileSymbol/StoryIndicator.js
+++ app/components/ProfileSymbol/StoryIndicator.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { View, Image, TouchableHighlight } from 'react-native';
-import Shimmer from 'react-native-shimmer';
 
 export default function StoryIndicator(props) {
   // Props = [ size: number, press ]
@@ -8,12 +7,10 @@ export default function StoryIndicator(props) {
     return (
         <TouchableHighlight onPress={props.press.bind(this)} style={{position: 'absolute', top: -12, left: -12}}>
           <View style={{aspectRatio: 1, position: 'relative', top: 0, left: 0}}>
-            <Shimmer direction={'up'}>
               <Image
                 source={require('../../assets/story-arrow.png')}
                 style={{height: props.size*1.5, width: props.size*1.5, opacity: 1}}
               />
-            </Shimmer>
           </View>
         </TouchableHighlight>
     );
